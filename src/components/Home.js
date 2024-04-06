95c882603e6f3dff1d08ee6ce91d8a54839147d1
Ahmad Omid <111872990+ahmadomid1984@users.noreply.github.com>
2024-04-06 19:17:22 +0300

    font size changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import Social from './Social'; // Import the Social component
 
 const styles = {
     nameStyle: {
-        fontSize: '10em',
+        fontSize: '6em',
         fontFamily: 'Times New Roman',
         marginBottom: '20px',
         textAlign: 'center',
@@ -16,7 +16,7 @@ const Home = () => {
         <div>
             <h1 style={styles.nameStyle}>Ahmad Omid</h1>
             <div>
-                <h2 style={{ fontSize: '3.5em', textAlign: 'center', marginBottom: '40px' }}>I'm a Developer & a Computer Applications Student</h2>
+                <h2 style={{ fontSize: '2.5em', textAlign: 'center', marginBottom: '40px' }}>I'm a Developer & a Computer Applications Student</h2>
             </div>
             <Social /> {/* Include the Social component here */}
         </div>
