68e19606b31445dfbfd82eaa5dd155a2efcd0fc6
Ahmad Omid <111872990+ahmadomid1984@users.noreply.github.com>
2024-06-17 14:14:17 +0300

    new project added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,11 +5,23 @@ const Projects = () => {
         <div className="container py-5">
             <h1 className="mb-4 text-center" style={{ fontSize: '4em' }}>Projects</h1>
             <div className="row justify-content-center">
+            <div className="col-lg-5 col-md-8 col-sm-12 mb-4 mt-5"> {/* Added col-sm-10 for smaller screens */}
+                    <div className="card h-100">
+                        <img src="images/creditRisk.png" className="card-img-top" alt="Project 0" />
+                        <div className="card-body text-center">
+                            <h5 className="card-title" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>Credit Risk Assessment</h5>
+                            <p className="card-text" style={{ fontSize: '1.2em' }}>Utilizing machine learning algorithms alongside with LightningChart, to predict and assesst the risks behind allocating credits.</p>
+                        </div>
+                        <div className="card-footer text-center">
+                            <a href="https://github.com/ahmadomid1984/Credit-Risk-Assessment" className="btn btn-primary">GitHub</a>
+                        </div>
+                    </div>
+                </div>
                 <div className="col-lg-5 col-md-8 col-sm-12 mb-4 mt-5"> {/* Added col-sm-10 for smaller screens */}
                     <div className="card h-100">
-                        <img src="images/Prediction.png" className="card-img-top" alt="Project 0" />
+                        <img src="images/Prediction.png" className="card-img-top" alt="Project 1" />
                         <div className="card-body text-center">
-                            <h5 className="card-title" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>Stock Market Prediction with LightningChart</h5>
+                            <h5 className="card-title" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>Stock Market Prediction</h5>
                             <p className="card-text" style={{ fontSize: '1.2em' }}>Utilizing machine learning algorithms alongside with LightningChart, showcases how to dynamically visualize stock trends and forecast future market movements efficiently.</p>
                         </div>
                         <div className="card-footer text-center">
@@ -19,7 +31,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-5 col-md-8 col-sm-12 mb-4 mt-5"> {/* Added col-sm-10 for smaller screens */}
                     <div className="card h-100">
-                        <img src="images/nodejs.png" className="card-img-top" alt="Project 1" />
+                        <img src="images/nodejs.png" className="card-img-top" alt="Project 2" />
                         <div className="card-body text-center">
                             <h5 className="card-title" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>React + Node.js Backend Website</h5>
                             <p className="card-text" style={{ fontSize: '1.2em' }}>Developed a robust backend for a car gallery website, using Node.js and MongoDB. Implemented full CRUD and API authentication functionalities.</p>
@@ -31,7 +43,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-5 col-md-8 col-sm-12 mb-4 mt-5"> {/* Added col-sm-10 for smaller screens */}
                     <div className="card h-100">
-                        <img src="images/Hotel-Waltikka.png" className="card-img-top" alt="Project 2" />
+                        <img src="images/Hotel-Waltikka.png" className="card-img-top" alt="Project 3" />
                         <div className="card-body text-center">
                             <h5 className="card-title" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>HAMK Design Factory Project</h5>
                             <p className="card-text" style={{ fontSize: '1.2em' }}>Designed an IoT interfaces integrating temperature, moisture, and CO2 data for Hotel Waltikka located in Valkeakoski.</p>
@@ -43,7 +55,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-5 col-md-8 col-sm-12 mb-4 mt-5"> {/* Added col-sm-10 for smaller screens */}
                     <div className="card h-100">
-                        <img src="images/portfolio.png" className="card-img-top" alt="Project 3" />
+                        <img src="images/portfolio.png" className="card-img-top" alt="Project 4" />
                         <div className="card-body text-center">
                             <h5 className="card-title" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>Dev Portfolio</h5>
                             <p className="card-text" style={{ fontSize: '1.2em' }}>A responsive Dev Portfolio Website made in React and Bootstrap.</p>
@@ -55,7 +67,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-5 col-md-8 col-sm-12 mb-4 mt-5"> {/* Added col-sm-10 for smaller screens */}
                     <div className="card h-100">
-                        <img src="images/bovi.png" className="card-img-top" alt="Project 1" />
+                        <img src="images/bovi.png" className="card-img-top" alt="Project 5" />
                         <div className="card-body text-center">
                             <h5 className="card-title" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>Bovi Website</h5>
                             <p className="card-text" style={{ fontSize: '1.2em' }}>Bovi, a specialized service centered around food and drinks, such as restaurants and cafes. Designed by Wordpress.</p>
@@ -66,7 +78,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-5 col-md-8 col-sm-12 mb-4 mt-5"> {/* Added col-sm-10 for smaller screens */}
                     <div className="card h-100">
-                        <img src="images/co2emission.png" className="card-img-top" alt="Project 4" />
+                        <img src="images/co2emission.png" className="card-img-top" alt="Project 6" />
                         <div className="card-body text-center">
                             <h5 className="card-title" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>AI & ML Projects</h5>
                             <p className="card-text" style={{ fontSize: '1.2em' }}>Working on various university projects, including supervised learning (KNN algorithm), unsupervised learning (k-means algorithm), Natural Language Processing (NLP) and etc...</p>
@@ -77,7 +89,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-5 col-md-8 col-sm-12 mb-4 mt-5"> {/* Added col-sm-10 for smaller screens */}
                     <div className="card h-100">
-                        <img src="images/automation.jpg" className="card-img-top" alt="Project 5" />
+                        <img src="images/automation.jpg" className="card-img-top" alt="Project 7" />
                         <div className="card-body text-center">
                             <h5 className="card-title" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>PDF Invoice Automation</h5>
                             <p className="card-text" style={{ fontSize: '1.2em' }}>Creating an automated PDF invoice processing system with UIPath and Robot Framework, enhancing efficiency and workflow integration.</p>
@@ -88,7 +100,7 @@ const Projects = () => {
                 </div>
                 <div className="col-lg-5 col-md-8 col-sm-12 mb-4 mt-5"> {/* Added col-sm-10 for smaller screens */}
                     <div className="card h-100">
-                        <img src="images/Coffeshop.png" className="card-img-top" alt="Project 6" />
+                        <img src="images/Coffeshop.png" className="card-img-top" alt="Project 8" />
                         <div className="card-body text-center">
                             <h5 className="card-title" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>Crush Cafe</h5>
                             <p className="card-text" style={{ fontSize: '1.2em' }}>Designing a website in different formats (HTML, Wordpress and PHP based in MySQL database).</p>
